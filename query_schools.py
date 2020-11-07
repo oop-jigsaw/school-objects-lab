eeda123d3848e0beb116d30615a598ac05a58d77
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-11-06 23:39:34 -0500

    remove finished code

--- query_schools.py
+++ query_schools.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from school import School
 
 def query_api():
     url = "https://raw.githubusercontent.com/jigsawlabs-student/decision-trees-intro/master/3-practical-ds-4/nyc_hs_sat.csv"
@@ -8,21 +7,14 @@ def query_api():
 
 
 def build_instances(school_dicts):
-    selected_attrs = ['dbn', 'name', 'boro', 'writing_score', 'math_avg', 
-        'reading_avg', 'total_students', 'attendance_rate']
-    schools = []
-    for school_dict in school_dicts:
-        selected_attr_dict = {k:v for k, v in school_dict.items() if k in selected_attrs}
-        school = School(**selected_attr_dict)
-        schools.append(school)
-    return schools
+    pass
 
 
 def largest_total_score_school(schools):
-    return max(schools, key=lambda school: school.total_score())
+    pass
 
 def average_total_score_of(schools):
-    return round(sum([school.total_score() for school in schools])/len(schools), 0)
+    pass
 
 def largest_average_student_attendance(schools):
-    return max(schools, key=lambda school: school.avg_student_attendance())
+    pass
